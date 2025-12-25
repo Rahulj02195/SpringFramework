3e5d471ba4b2900047e83d25325b516a271e4d3d
Rahulj02195 <rahul2539raj123@gmail.com>
2025-12-25 11:29:06 +0530

    Spring core project

---
+++ src/main/java/com/spring/core/Library.java
@@ -0,0 +1,16 @@
+package com.spring.core;
+
+import java.util.List;
+
+public class Library {
+	private List<Book> books;
+
+	public List<Book> getBook() {
+		return books;
+	}
+
+	public void setBook(List<Book> books) {
+		this.books = books;
+	}
+
+}
